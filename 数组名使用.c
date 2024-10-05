4e31de79d7fee19ce48dc483796988c78481a7b9
zjuwangrui <chinesewangrui@zju.edu.cn>
2024-10-05 19:51:37 +0800

    first commit

---
+++ 数组名使用.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void PrintArray(int array[]){
+     for(int i=0;i<3;i++){
+        printf("%d ",*array);
+        array++;
+     }
+}
+int main(){
+    int array[]={1,2,3};
+    PrintArray(array);
+    return 0;
+}//数组名做指针变量
+int main(){
+    int a[]={1,2,3};
+    int b[3]={0};
+    printf("%d",a);
+    a++;
+    printf("%d",*a);
+    return 0;
+}//数组名++何时合法
